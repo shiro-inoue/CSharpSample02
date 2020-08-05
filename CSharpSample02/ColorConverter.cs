85848c71ba392c968e12f5bd4b1639dae9d29707
shiro-inoue <shiro_inoue@sobal.co.jp>
2020-08-05 16:25:52 +0900

    動作確認済み版

---
+++ CSharpSample02/ColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CSharpSample02
+{
+    public class ColorConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            byte R = (byte)(double)(values[0]);
+            byte G = (byte)(double)(values[1]);
+            byte B = (byte)(double)(values[2]);
+
+            return Color.FromRgb(R, G, B);
+        }
+
+        //-----------------------------------------------------------------------------------------------
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    } // end of ColorConverter class
+}
