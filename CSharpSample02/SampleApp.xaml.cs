d5c8bc2cda9c64b5751d51c0527ab7fb85371fc9
shiro-inoue <shiro_inoue@sobal.co.jp>
2020-08-18 13:44:07 +0900

    最大化最小化ボタンの消去

--- CSharpSample02/SampleApp.xaml.cs
+++ CSharpSample02/SampleApp.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -24,5 +26,27 @@ namespace CSharpSample02
         {
             InitializeComponent();
         }
+
+        #region "最大化・最小化ボタンの非表示設定"
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        const int GWL_STYLE = -16;
+        const int WS_SYSMENU = 0x30000;
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            IntPtr handle = new WindowInteropHelper(this).Handle;
+            int style = GetWindowLong(handle, GWL_STYLE);
+            style = style & (~WS_SYSMENU);
+            SetWindowLong(handle, GWL_STYLE, style);
+        }
+
+        #endregion
     }
 }
